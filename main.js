0e2cbed0bfcba7406fd345b84bd91a77eb2f3045
Vedant2021-hub <82112467+Vedant2021-hub@users.noreply.github.com>
2022-02-22 08:14:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    background('#FF2E2E')
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference + "px";
+    textSize(difference);
+    fill('#FFE787');
+    text('Vedant', 50, 400);
+}
+
+function modelLoaded() {
+    console.log('Pose Net is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+    }
+}
